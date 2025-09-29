36111f71952050095291a234503b5095f63d71e0
Tuna <anhjack1102@gmail.com>
2025-09-29 10:06:54 +0700

    test dp render

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+// Test route
+app.get("/", (req, res) => {
+    res.send("API backend is running 🚀")
+})
+
 // Routes
 app.use("/api/v1/user", userRoute)
 app.use("/api/v1/roomCategory", roomCategoryRoute)
